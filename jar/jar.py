6f1e8de67026177a2a670368c20fcfcdf7f87235
san5216 <mster.norris@gmail.com>
2022-11-25 14:54:20 -0600

    New project

--- jar/jar.py
+++ jar/jar.py
@@ -6,46 +6,77 @@ class Jar:
 
         :param capacity: int
         """
-
-        if capacity < 0:
-            raise ValueError
+        self.capacity = capacity
+        self._size = 0
+        ...
+        # self.size = ""
+        # self.capacity = capacity
 
     def __str__(self):
         """Return a string with n 🍪 cookies, where n is the number of cookies in the jar
 
         :return: string of 🍪s
         """
-        ...
+        return "🍪" * self.size
 
     def deposit(self, n):
         """Add n cookies to the cookie jar.
 
         :param n: integer
         """
+        self.size += n
+        return self.size
 
-        size += n
-        # Raise a ValueError if adding that many cookies would exceed the jar's capacity.
-        ...
 
     def withdraw(self, n):
         """Remove n cookies from the cookie jar.
 
         :param n: integer
         """
-
-        # Raise a ValueError if there aren't enough cookies in the cookie jar.
-        ...
-
-    @property
-    def capacity(self):
-        return self.capacity
-
-    @property
-    def size(self):
+        self.size -= n
         return self.size
 
 
-jar = Jar(capacity=10)
-print(str(Jar))
-print(jar.capacity)
-print(jar.size)
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @capacity.setter
+    def capacity(self, capacity):
+        if capacity < 0:
+            raise ValueError
+        self._capacity = capacity
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if size < 0:
+            raise ValueError
+
+        if self.capacity < size:
+            raise ValueError
+
+        self._size = size
+
+
+def main():
+    jar = Jar(capacity=10)
+    jar.deposit(3)
+    print(f"capacity: {jar.capacity}")
+    print(f"size: {jar.size}")
+    print(str(jar))
+    jar.withdraw(3)
+    print(f"capacity: {jar.capacity}")
+    print(f"size: {jar.size}")
+    print(str(jar))
+    jar.withdraw(4)
+    print(f"capacity: {jar.capacity}")
+    print(f"size: {jar.size}")
+    print(str(jar))
+
+if __name__ == '__main__':
+    main()
--- jar/test_jar.py
+++ jar/test_jar.py
@@ -8,21 +8,21 @@ def test_init():
 
 
 def test_str():
-    jar = Jar(capacity=12)
+    jar = Jar()
     jar.deposit(3)
     assert str(jar) == "🍪🍪🍪"
 
 
 def test_deposit():
-    jar = Jar(capacity=12)
+    jar = Jar()
     jar.deposit(7)
-    assert jar.size() == 7
+    assert jar.size == 7
     with pytest.raises(ValueError):
         assert jar.deposit(13)
 
 
 def test_withdraw():
-    jar = Jar(capacity=12)
+    jar = Jar()
     jar.deposit(7)
     assert jar.withdraw(5) == 2
     with pytest.raises(ValueError):
